b2298bc58f26b98366a5e882c89b2b57b34a8541
khb6997@gmail.com <khb6997@gmail.com>
2023-02-01 16:49:19 +0900

    프로젝트 시작 - 23.02.01

---
+++ src/main/java/com/bong/DailyShareService/DailyShareServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bong.DailyShareService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyShareServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyShareServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bong/DailyShareService/DailyShareServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bong.DailyShareService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyShareServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
