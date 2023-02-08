214be16f5a631250e34f3aa82cfdfaa8f58f4420
Meojung1624 <123927059+Meojung1624@users.noreply.github.com>
2023-02-08 15:14:35 +0900

    인덱스 페이지 틀 구성

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,9 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function Tbody(){
+  return <tbody>
+      <tr>
+        <td>_</td>
+        <td>_</td>
+        <td>_</td>
+        <td>_</td>
+        <td>_</td>
+      </tr>
+    </tbody>
+}
+
+function Thead(){
+  return <thead>
+      <tr>
+        <th>번호</th>
+        <th>제목</th>
+        <th>작성자</th>
+        <th>작성일</th>
+        <th>조회수</th>
+      </tr>
+    </thead>
+}
+
 function App() {
+  let [maxpage,setmaxpage] = useState(10);
+
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +45,35 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+      <h1 className = "title">
+      Daily Share<div align = "right">🤷‍♀️</div>
+      
+      </h1 >
+      <p align = "right">
+        <strong><a id="/oauth2/authorization/google">로그인</a></strong>   
+      </p>
+  
+      <table width = "1500px"  height = "500px" margin = "500px">
+        <Thead></Thead>
+        <Tbody></Tbody>
+        <Tbody></Tbody>
+        <Tbody></Tbody>
+        <Tbody></Tbody>
+        <Tbody></Tbody>
+        <Tbody></Tbody>
+        <Tbody></Tbody>
+        <Tbody></Tbody>
+        <Tbody></Tbody>
+        <Tbody></Tbody>
+      </table>
+      <a href=''> 1 </a>
+      <a href=''> 2 </a>
+      <a href=''> 3 </a>
+      <a href=''> 4 </a>
+      <a href=''> 5 </a>
+      <button type = "button" onClick="location.href=''">글작성</button>      
+>>>>>>> Stashed changes
     </div>
   );
 }
