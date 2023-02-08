efc07c64ad337f9335134a0f8e89d24518c571cd
khb6997@gmail.com <khb6997@gmail.com>
2023-02-08 16:46:06 +0900

    각 하위브랜치 dev로 통합

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,81 +1,20 @@
-import React, { useState } from 'react';
-import './App.css';
-
-function Tbody(){
-  return <tbody>
-      <tr>
-        <td>_</td>
-        <td>_</td>
-        <td>_</td>
-        <td>_</td>
-        <td>_</td>
-      </tr>
-    </tbody>
-}
-
-function Thead(){
-  return <thead>
-      <tr>
-        <th>번호</th>
-        <th>제목</th>
-        <th>작성자</th>
-        <th>작성일</th>
-        <th>조회수</th>
-      </tr>
-    </thead>
-}
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
-  let [maxpage,setmaxpage] = useState(10);
+   const [hello, setHello] = useState('')
 
-  return (
-    <div className="App">
-<<<<<<< Updated upstream
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jss</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
-      <h1 className = "title">
-      Daily Share<div align = "right">🤷‍♀️</div>
-      
-      </h1 >
-      <p align = "right">
-        <strong><a id="/oauth2/authorization/google">로그인</a></strong>   
-      </p>
-  
-      <table width = "1500px"  height = "500px" margin = "500px">
-        <Thead></Thead>
-        <Tbody></Tbody>
-        <Tbody></Tbody>
-        <Tbody></Tbody>
-        <Tbody></Tbody>
-        <Tbody></Tbody>
-        <Tbody></Tbody>
-        <Tbody></Tbody>
-        <Tbody></Tbody>
-        <Tbody></Tbody>
-        <Tbody></Tbody>
-      </table>
-      <a href=''> 1 </a>
-      <a href=''> 2 </a>
-      <a href=''> 3 </a>
-      <a href=''> 4 </a>
-      <a href=''> 5 </a>
-      <button type = "button" onClick="location.href=''">글작성</button>      
->>>>>>> Stashed changes
-    </div>
-  );
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => setHello(response.data))
+        .catch(error => console.log(error))
+    }, []);
+
+    return (
+        <div>
+            백엔드에서 가져온 데이터입니다 : {hello}
+        </div>
+    );
 }
 
 export default App;
