3dd49f7d5286833911faf1c1b49443c262560f03
dlrlgns <dlrlgns1104@naver.com>
2023-02-08 17:08:18 +0900

    데이터 수집 및 라벨링(2/3)

---
+++ src/main/ai/food_data_0208.py
@@ -0,0 +1,20 @@
+def crawling(soup) :
+    content = []
+    div = soup.find_all("span", class_="se-fs- se-ff-")
+    for i in range(len(div)):
+        if (div[i].get_text() == "\u200b") :
+            continue
+        content.append(div[i].get_text())
+    result = content
+    return result
+
+list_href = []
+list_content = []
+for i in range(12,60):    
+    url = "https://www.menupan.com/restaurant/goodrest/goodrest_view.asp?id=1843"+str(i)+"&rurl=%2FRestaurant%2FGoodRest%2FGoodRest%5Flist%2Easp%3Fmn%3Df%26ds%3Dc%26tc%3D28516%26pg%3D10"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
+    #print(list_href)
+    result = crawling(soup)
+    list_content.append(result)
+print(list_content)
