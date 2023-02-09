8324a4351f4896a9c597a925dbc370f02185a986
khb6997@gmail.com <khb6997@gmail.com>
2023-02-09 13:50:17 +0900

    수정

--- src/test/java/com/bong/DailyShareService/domain/comments/CommentsRepositoryTest.java
+++ src/test/java/com/bong/DailyShareService/domain/comments/CommentsRepositoryTest.java
@@ -20,52 +20,52 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 public class CommentsRepositoryTest {
 
-    @Autowired
-    CommentsRepository commentsRepository;
-
-    @After
-    public void clear() {
-        commentsRepository.deleteAll();
-    }
-
-    @Test
-    public void load_comment() {
-
-        //given
-        String comment = "테스트 댓글";
-
-        User user = User.builder()
-                .nickname("테스트 유저")
-                .email("테스트 이메일")
-                .role(Role.USER)
-                .picture("테스트 사진")
-                .build();
-
-        Posts posts = Posts.builder()
-                .title("테스트 게시글")
-                .content("테스트")
-                .user(user)
-                .build();
-
-        Comments comments = Comments.builder()
-                .comment(comment)
-                .posts(posts)
-                .user(user)
-                .build();
-
-        commentsRepository.save(comments);
-
-        //when
-        List<Comments> commentsList = commentsRepository.findAll();
-
-        //then
-        Comments testComments = commentsList.get(0);
-
-        assertThat(testComments.getComment()).isEqualTo(comment);
-        assertThat(testComments.getPosts().getTitle()).isEqualTo("테스트 게시글");
-        assertThat(testComments.getPosts().getContent()).isEqualTo("테스트");
-        assertThat(testComments.getUser().getNickname()).isEqualTo("테스트 유저");
-        assertThat(testComments.getUser().getEmail()).isEqualTo("테스트 이메일");
-
-    }
+//    @Autowired
+//    CommentsRepository commentsRepository;
+//
+//    @After
+//    public void clear() {
+//        commentsRepository.deleteAll();
+//    }
+//
+//    @Test
+//    public void load_comment() {
+//
+//        //given
+//        String comment = "테스트 댓글";
+//
+//        User user = User.builder()
+//                .nickname("테스트 유저")
+//                .email("테스트 이메일")
+//                .role(Role.USER)
+//                .picture("테스트 사진")
+//                .build();
+//
+//        Posts posts = Posts.builder()
+//                .title("테스트 게시글")
+//                .content("테스트")
+//                .user(user)
+//                .build();
+//
+//        Comments comments = Comments.builder()
+//                .comment(comment)
+//                .posts(posts)
+//                .user(user)
+//                .build();
+//
+//        commentsRepository.save(comments);
+//
+//        //when
+//        List<Comments> commentsList = commentsRepository.findAll();
+//
+//        //then
+//        Comments testComments = commentsList.get(0);
+//
+//        assertThat(testComments.getComment()).isEqualTo(comment);
+//        assertThat(testComments.getPosts().getTitle()).isEqualTo("테스트 게시글");
+//        assertThat(testComments.getPosts().getContent()).isEqualTo("테스트");
+//        assertThat(testComments.getUser().getNickname()).isEqualTo("테스트 유저");
+//        assertThat(testComments.getUser().getEmail()).isEqualTo("테스트 이메일");
+//
+//    }
 }
