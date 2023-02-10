f9bac4ef4fe0eadf6dc070410c8ade8ef22a7f9c
Meojung1624 <rlatpwnd8321@naver.com>
2023-02-10 13:55:49 +0900

    페이징 구성 중간까지

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,8 +1,67 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import './App.css';
+
+function Thead(){
+  return <thead>
+      <tr>
+        <th>번호</th>
+        <th>제목</th>
+        <th>작성자</th>
+        <th>작성일</th>
+        <th>조회수</th>
+      </tr>
+    </thead>
+}
+function Tbody(props){
+  const list = []
+  for(let i = 0; i < props.D_B.length; i++){
+    let t = props.D_B[i]
+    list.push(
+    <>
+    <tr>
+      <td>{t.id}</td>
+      <td><a href=''>{t.title}</a></td>
+      <td>{t.writer}</td>
+      <td>{t.date}</td>
+      <td>{t.click}</td>
+    </tr>
+    </>
+    )
+  }
+  return <tbody>{list}</tbody>
+}
+
+function A(props){                                                                    //페이지숫자 컴포넌트 게시글 개수에 맞춰 페이지 숫자가 생성되고 누를때마다 현페이를 나타내슨 변수를 변경하도록 함
+  const index = []
+  let pagesu = 0
+  if(props.maximum%10 === 0){pagesu = props.maximum/10}
+  else{pagesu = props.maximum/10 +1}
+  for(let i = 1; i <= pagesu; i++){
+    index.push(<a href = {i}>{i}</a>)
+  }
+  return <>{index}</>
+}
 
 function App() {
-   const [hello, setHello] = useState('')
+  const D_B = [                                                                              //DB역활 이후 수정 페이지 이동구현 완료후 제거
+    {id:1, title:"제대하고싶은 상준이", writer:"상준서", date:20001101, click:5},
+    {id:2, title:"비트코인 떡상하는 법", writer:"루나", date:20200917, click:200},
+    {id:3, title:"인수인계하는법", writer:"박크승도", date:2023209, click:200},
+    {id:4, title:"ㄱ", writer:"루나ㄱ", date:10200917, click:100},
+    {id:5, title:"ㄴ", writer:"루나ㄴ", date:20200917, click:200},
+    {id:6, title:"ㄷ", writer:"루나ㄷ", date:3200917, click:300},
+    {id:7, title:"ㄹ", writer:"루나ㄹ", date:40200917, click:400},
+    {id:8, title:"ㅁ", writer:"루나ㅁ", date:50200917, click:500},
+    {id:9, title:"ㅂ", writer:"루나ㅂ", date:60200917, click:600},
+    {id:10, title:"ㅅ", writer:"루나ㅅ", date:70200917, click:700},
+    {id:11, title:"ㅇ", writer:"루나ㅇ", date:80200917, click:800}
+  ]
+  const [info,getinfo] = useState()
+
+  const [maximum,setmaximum] = useState(41)                                                //!!!!함수로 게시판 개수 가져오기
+  const [page,setpage] = useState(0)                                                      //밑에 a태그를 누르면 같이 적용되도록 구현 (현재 1로 가정!!)
+  const [hello, setHello] = useState('')
 
     useEffect(() => {
         axios.get('/api/hello')
@@ -11,9 +70,29 @@ function App() {
     }, []);
 
     return (
-        <div>
-            백엔드에서 가져온 데이터입니다 : {hello}
-        </div>
+      <div className="App">
+      <header>
+        <h1 className = "title">
+        Daily Share
+        <div align = "right">🤷‍♀️</div>
+        </h1 > 
+      </header>
+      
+      <p align = "right">
+        <strong><a id="/oauth2/authorization/google">로그인</a></strong>   
+      </p>
+  
+      <table width = "1500px"  height = "100px" margin = "500px">
+        <Thead></Thead>
+        <Tbody D_B={D_B}></Tbody>
+      </table>
+      <A maximum={maximum}></A>
+      <button type = "button" onClick="location.href=''">글작성</button> 
+
+      <p>
+        백엔드에서 가져온 데이터입니다 : {hello}  
+      </p>     
+    </div>
     );
 }
 
