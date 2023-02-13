ac2e3d2b6c551d72b007ceb0ddadb22012cd6ccd
khb6997@gmail.com <khb6997@gmail.com>
2023-02-13 15:41:58 +0900

    React, Springboot 연결 및 프록시 설정

--- src/main/java/com/bong/DailyShareService/web/IndexController.java
+++ src/main/java/com/bong/DailyShareService/web/IndexController.java
@@ -1,44 +1,44 @@
-package com.bong.DailyShareService.web;
-
-import com.bong.DailyShareService.config.auth.LoginUser;
-import com.bong.DailyShareService.config.auth.dto.SessionUser;
-import com.bong.DailyShareService.service.PostsService;
-import com.bong.DailyShareService.web.dto.posts.PostsResponseDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-@RequiredArgsConstructor
-@Controller
-public class IndexController {
-
-    private final PostsService postsService;
-
-    @GetMapping("/")
-    public String index(Model model, @LoginUser SessionUser user) {
-        model.addAttribute("posts", postsService.findAllDesc());
-        if (user != null) {
-            model.addAttribute("user", user);
-        }
-        return "index";
-    }
-
-    @GetMapping("/posts/save")
-    public String postsSave(Model model, @LoginUser SessionUser user) {
-        if (user != null) {
-            model.addAttribute("user", user);
-        }
-        return "posts-save";
-    }
-
-    @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model) {
-        PostsResponseDto dto = postsService.findById(id);
-        postsService.increaseViewCount(id);
-        model.addAttribute("post", dto);
-
-        return "posts-update";
-    }
-}
+//package com.bong.DailyShareService.web;
+//
+//import com.bong.DailyShareService.config.auth.LoginUser;
+//import com.bong.DailyShareService.config.auth.dto.SessionUser;
+//import com.bong.DailyShareService.service.PostsService;
+//import com.bong.DailyShareService.web.dto.posts.PostsResponseDto;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//
+//@RequiredArgsConstructor
+//@Controller
+//public class IndexController {
+//
+//    private final PostsService postsService;
+//
+//    @GetMapping("/")
+//    public String index(Model model, @LoginUser SessionUser user) {
+//        model.addAttribute("posts", postsService.findAllDesc());
+//        if (user != null) {
+//            model.addAttribute("user", user);
+//        }
+//        return "index";
+//    }
+//
+//    @GetMapping("/posts/save")
+//    public String postsSave(Model model, @LoginUser SessionUser user) {
+//        if (user != null) {
+//            model.addAttribute("user", user);
+//        }
+//        return "posts-save";
+//    }
+//
+//    @GetMapping("/posts/update/{id}")
+//    public String postsUpdate(@PathVariable Long id, Model model) {
+//        PostsResponseDto dto = postsService.findById(id);
+//        postsService.increaseViewCount(id);
+//        model.addAttribute("post", dto);
+//
+//        return "posts-update";
+//    }
+//}
