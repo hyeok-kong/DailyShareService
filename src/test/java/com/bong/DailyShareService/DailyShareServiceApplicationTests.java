b0835ff5fe33ca94dcff1a1440934d85a06dbe4b
meojung <rlatpwnd8321@naver.com>
2023-02-21 16:56:39 +0900

    로그인 준비 작업

--- src/test/java/com/bong/DailyShareService/DailyShareServiceApplicationTests.java
+++ src/test/java/com/bong/DailyShareService/DailyShareServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.bong.DailyShareService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class DailyShareServiceApplicationTests {
 
 	@Test
